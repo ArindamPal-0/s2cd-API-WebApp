04ea4512dd8f527914f43e1330c8f20052a6d74c
Arindam Pal <arindampal2000@gmail.com>
2021-11-13 18:38:51 +0530

    enabled cross site access using flask-cors
    installed and added flask-cors
    recreated requirements.txt to add flask-cors install requirement

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 from log import log, Log
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
